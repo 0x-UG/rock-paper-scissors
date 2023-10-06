9f5b78e50ad3d15e29f46dfb90516ee4946faef3
0x-UG <okekecee12@gmail.com>
2023-10-06 14:58:45 +0100

    finished for the most part but still have some bugs to fix

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
+function playRound(playerSelection, computerSelection) {
+
+      if (playerSelection === "Rock") {
+      if (computerSelection === "Rock") {
+        alert("This is a tie, no winners here! You both picked Rock");
+      } else if (computerSelection === "Paper") {
+        alert("Computer wins! Paper covers Rock");
+      } else if (computerSelection === "Scissors") {
+        alert("Player wins! Rock breaks Scissors");
+      } else {
+        alert("Invalid computer selection.");
+      }
+    } else if (playerSelection === "Paper") {
+      if (computerSelection === "Rock") {
+        alert("Player wins! Paper covers Rock");
+      } else if (computerSelection === "Paper") {
+        alert("This is a tie, no winners here! You both picked Paper");
+      } else if (computerSelection === "Scissors") {
+        alert("Computer Wins! Scissors cuts Paper");
+      } else {
+        alert("Invalid computer selection.");
+      }
+    } else if (playerSelection === "Scissors") {
+      if (computerSelection === "Rock") {
+        alert("Computer Wins! Rock breaks Scissors");
+      } else if (computerSelection === "Paper") {
+        alert("Player wins! Scissors cuts Paper");
+      } else if (computerSelection === "Scissors") {
+        alert("This is a tie, no winners here! You both picked Scissors");
+      } else {
+        alert("Invalid computer selection.");
+      }
+    } else {
+      alert("Invalid player selection.");
+    }
+  }
+  
+
+
 //code to ask user to input string between rock paper and scissors
 let playerInput = prompt("Pick one between rock, paper and scissors: ");
 //code to make player selection upper case only at the first letter to compare with the computer choice
 let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
-
 
 //created fxn that picks a random word btw rock paper and scissors using random index
 function getComputerChoice () {
@@ -18,13 +56,4 @@ function getComputerChoice () {
 
 //calls the get computer choice fxn so the computer can select a random choice
 const computerSelection = getComputerChoice();
-
-function playRound (playerSelection, computerSelection) {
-    if (playerSelection === "Rock" && computerSelection === "Rock") {
-        alert("This is a tie, no winners here! You both picked rock");
-    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
-        alert("This is a tie, no winners here! You both picked paper");
-    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
-        alert("This is a tie, no winners here! You both picked scissors");
-    }
-}
+alert(playRound(playerSelection, computerSelection));
