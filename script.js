59e23cbd21284c12e7d6b17cf081c0425acbecdf
0x-UG <okekecee12@gmail.com>
2023-10-06 13:53:06 +0100

    created the fxn that gets the players input as well

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let playerSelection = prompt("Pick one between rock, paper and scissors: ");
+//code to ask user to input string between rock paper and scissors
+let playerInput = prompt("Pick one between rock, paper and scissors: ");
+//code to make player selection upper case only at the first letter to compare with the computer choice
+let playerSelection = playerInput.charAt(0).toUpperCase() + playerInput.slice(1).toLowerCase();
 
 
 //created fxn that picks a random word btw rock paper and scissors using random index
@@ -13,5 +16,15 @@ function getComputerChoice () {
     }
 }
 
+//calls the get computer choice fxn so the computer can select a random choice
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
+
+function playRound (playerSelection, computerSelection) {
+    if (playerSelection === "Rock" && computerSelection === "Rock") {
+        alert("This is a tie, no winners here! You both picked rock");
+    } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+        alert("This is a tie, no winners here! You both picked paper");
+    } else if (playerSelection === "Scissors" && computerSelection === "Scissors") {
+        alert("This is a tie, no winners here! You both picked scissors");
+    }
+}
